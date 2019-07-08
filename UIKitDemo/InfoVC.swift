78d9d043f91bfde38eea35f3385d0281ae167280
wuufone <wuufone@gmail.com>
2019-07-08 17:56:25 +0800

    17. 為「帳戶資訊」加入 InfoVC，並加入「登出」鈕、建立其 IBAction。

---
+++ UIKitDemo/InfoVC.swift
@@ -0,0 +1,33 @@
+//
+//  InfoVC.swift
+//  UIKitDemo
+//
+//  Created by wuufone on 2019/7/8.
+//  Copyright © 2019 江武峯. All rights reserved.
+//
+
+import UIKit
+
+class InfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logoutButtonAction(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
