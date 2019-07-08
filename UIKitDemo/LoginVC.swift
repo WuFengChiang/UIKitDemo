3279b22881b2417e2011004e7e23b13601fc89c3
wuufone <wuufone@gmail.com>
2019-07-08 16:21:16 +0800

    1. 使用 Rename 重構第一個 ViewController.

--- UIKitDemo/LoginVC.swift
+++ UIKitDemo/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginVC.swift
 //  UIKitDemo
 //
 //  Created by wuufone on 2019/7/8.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
